227a11a9acf3cd686346e8b03d7e6692c624082f
Nadine Omotese <nadine.omotese@tipico.com>
2019-04-25 09:55:36 +0200

    End of lecture 77 set up basic routing with ReactRouter

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/Indecision';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is my ExpenseDashboardPage
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is my ExpenseComponent
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        This is my EditExpensePage
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is my HelpPage
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} />
+            <Route path="/create" component={AddExpensePage}/>
+            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/help" component={HelpPage}/>
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
