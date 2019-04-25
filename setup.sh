ac89143ee64e086feec21d495ea26aefee86d256
Nadine Omotese <nadine.omotese@tipico.com>
2019-04-25 17:53:29 +0200

    End of lecture 80, splitting up the components

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #open text editor
-code --folder-uri public
+code .
 
 #open prefered browser with url
-firefox -new-window https://bx-watchmen.udemy.com &
 #firefox https://reactjs.org/docs/getting-started.html
 firefox -new-window http://127.0.0.1:8080 &
+firefox -new-window https://bx-watchmen.udemy.com &
 
 yarn run dev-server
