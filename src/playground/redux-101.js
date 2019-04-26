855fc311c3e78edbee6dce074b7648ebbe8ca46c
Nadine Omotese <nadine.omotese@tipico.com>
2019-04-26 16:10:45 +0200

    fix merge problem

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,10 +1,6 @@
 import { createStore } from 'redux';
 
-<<<<<<< Updated upstream
 const store = createStore((state = { count: 0}) => {
-=======
-const store = createStore((state = { count: 10}) => {
->>>>>>> Stashed changes
     return state;
 });
 
