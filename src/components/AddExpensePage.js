32a03b9f700839ac00c5e0d2044d7d9db80b4f1d
Nadine Omotese <nadine.omotese@tipico.com>
2019-04-26 11:44:08 +0200

    End of lecture 85, finished routing example

--- src/components/AddExpensePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AddExpensePage = () => (
-    <div>
-        This is my ExpenseComponent
-    </div>
-);
-
-export default AddExpensePage;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 //entry point -> output
 module.exports = {
-   entry: './src/app.js',
+   entry: './src/playground/redux-101.js',
+   // entry: './src/app.js',
    output: {
       path: path.join(__dirname, 'public'),
       filename: 'bundle.js'
