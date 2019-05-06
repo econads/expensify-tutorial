c45efd2ff5c155c8a3baf932cb470ff97c6beba4
Nadine Omotese <nadine.omotese@tipico.com>
2019-05-06 09:55:05 +0200

    End of lecture 87, subscribing and dynamic actions (redux)

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,71 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0}) => {
-    return state;
+const store = createStore((state = { count: 0}, action) => {
+    console.log('running');
+
+    switch (action.type){
+        case 'INCREMENT':
+        const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+        return {
+            count: state.count + incrementBy
+        };
+        case 'DECREMENT':
+        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+        return {
+            count: state.count - decrementBy
+        };
+        case 'SET':
+        //This is supposed to force the type somehow
+        return {
+            count: action.count
+        };
+        case 'RESET':
+        return {
+            count: 0
+        };
+        default: 
+        return state;
+    }
 });
 
-console.log(store.getState());
+//returns unsubscribe function
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(
+    {
+        type: 'INCREMENT',
+        incrementBy: 5
+    }
+);
+
+store.dispatch(
+    {
+        type: 'INCREMENT'
+    }
+);
+
+store.dispatch(
+    {
+        type: 'RESET'
+    }
+);
+
+store.dispatch(
+    {
+        type: 'DECREMENT'
+    }
+);
+
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
+
+store.dispatch(
+    {
+        type: 'DECREMENT',
+        decrementBy: 3
+    }
+);
