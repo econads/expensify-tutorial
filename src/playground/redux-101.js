91bacd821454703ecab3463d74a07b86cecb9c45
Nadine Omotese <nadine.omotese@tipico.com>
2019-05-10 12:26:44 +0200

    End of Lecture 90, reorganising.  Making action generators for each action.

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,10 +1,33 @@
 import { createStore } from 'redux';
 
 //Action generators
-const incrementCount = (payload = {}) => ({
+
+// const add = ({a, b}, c) => {
+//     return a + b + c;
+// }
+// console.log(add({a:1, b:2}, 3 ));
+
+//get incrementBy from the passed in object which is the {}.  Default to 1 which is the = 1.
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
     type: 'INCREMENT',
-    incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+    //The object name is the same as the param name, so we can just use the param directly.
+    incrementBy
 });
+
+const decrementCount = ( {decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+const setCount = ( {count} = {}) => ({
+    type: 'SET',
+    count
+});
+
 
 const store = createStore((state = { count: 0}, action) => {
     console.log('running');
@@ -15,9 +38,8 @@ const store = createStore((state = { count: 0}, action) => {
             count: state.count + action.incrementBy
         };
         case 'DECREMENT':
-        const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
         return {
-            count: state.count - decrementBy
+            count: state.count - action.decrementBy
         };
         case 'SET':
         //This is supposed to force the type somehow
@@ -49,26 +71,25 @@ store.dispatch(incrementCount({ incrementBy: 5 }));
 
 store.dispatch(incrementCount());
 
-store.dispatch(
-    {
-        type: 'RESET'
-    }
-);
+store.dispatch(resetCount());
+// store.dispatch(
+//     {
+//         type: 'RESET'
+//     }
+// );
 
-store.dispatch(
-    {
-        type: 'DECREMENT'
-    }
-);
+store.dispatch(decrementCount());
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-})
+store.dispatch(setCount({count: 101}));
+// store.dispatch({
+//     type: 'SET',
+//     count: 101
+// })
 
-store.dispatch(
-    {
-        type: 'DECREMENT',
-        decrementBy: 3
-    }
-);
+store.dispatch(decrementCount({decrementBy: 3}));
+// store.dispatch(
+//     {
+//         type: 'DECREMENT',
+//         decrementBy: 3
+//     }
+// );
